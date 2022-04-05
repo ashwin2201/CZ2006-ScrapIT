cf92a0d8513006b8b79ac66fcefc91677b9ff11b
neelkumar812 <neelkumar812@gmail.com>
2022-04-06 00:39:21 +0800

    Verification page update

--- lib/homeScreen/ui/Verification.dart
+++ lib/homeScreen/ui/Verification.dart
@@ -1,11 +1,10 @@
+import 'dart:io';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:scrap_it/homeScreen/ui/home_screen.dart';
-import 'dart:math';
+
 
 class VerificationPage extends StatelessWidget {
-  //const CongratsPage({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,6 +24,7 @@ class VerificationPage extends StatelessWidget {
 Widget verificationPage(BuildContext context) {
   return ListView(
     children: <Widget>[
+
       SafeArea(
         child: Container(
           padding: const EdgeInsets.all(20),
@@ -39,6 +39,7 @@ Widget verificationPage(BuildContext context) {
           ),
         ),
       ),
+
       const Expanded(
         child: CircleAvatar(
           radius: 100,
@@ -49,27 +50,21 @@ Widget verificationPage(BuildContext context) {
           ),
         ),
       ),
+
       Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.only(top: 15),
-          padding: const EdgeInsets.all(10),
-          child: ElevatedButton(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(top: 15),
+        padding: const EdgeInsets.all(10),
+        child: ElevatedButton(
             style: ElevatedButton.styleFrom(
                 minimumSize: const Size(225, 50),
                 textStyle: const TextStyle(fontSize: 22),
                 primary: Colors.green),
             child: const Text('Upload Image'),
-            onPressed: () => Navigator.pop(context),
+            onPressed: () =>cameraPopup(context),
+        ),
+      ),
 
-
-
-
-
-
-          )),
-
-
-          
       SafeArea(
         child: Container(
           margin: const EdgeInsets.only(top: 5),
@@ -86,7 +81,89 @@ Widget verificationPage(BuildContext context) {
             ),
           ),
         ),
-      ),
+      ),      
     ],
+  );
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+cameraPopup(BuildContext context) {
+
+  //hardcoding state need to make this all stateful later
+  int state = 0;
+
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+            child: Container(
+              height: 190,
+              width: 200,
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                children: <Widget> [
+                  const Text(
+                      'Select one of the options below',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  
+
+                  const Spacer(flex: 1),
+
+
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        
+                        minimumSize: const Size(225, 50),
+                        textStyle: const TextStyle(fontSize: 22),
+                        backgroundColor: Colors.green,
+                        primary: Colors.black),
+                    child: const Text(
+                      'Take Picture from Camera',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    onPressed: (){
+                      state = 1;
+                      Navigator.pop(context);
+
+                    }
+                  ),
+
+
+                  const Spacer(flex: 3),
+
+
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        minimumSize: const Size(225, 50),
+                        textStyle: const TextStyle(fontSize: 22),
+                        backgroundColor: Colors.green,
+                        primary: Colors.black),
+                    child: const Text(
+                      'Select from Gallery',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    onPressed: (){
+                      state = 2;
+                      Navigator.pop(context);
+                    }
+                  ),
+
+
+                ],
+              ),
+            )
+          );
+      }
   );
 }
